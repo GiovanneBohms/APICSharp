6fbdbf8a69dbc26f960fbccbeb53e9e6d6f11de2
Giovanne <giovannebohms@gmail.com>
2024-07-31 15:58:11 -0300

    LinqFilter

---
+++ ScreenSound - 04/Filtros/LinqFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ScreenSound___04.Modelos;
+
+namespace ScreenSound___04.Filtros
+{
+    internal class LinqFilter
+    {
+        public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+        {
+            var todosOsGenerosMusicais = musicas.Select(musicas => musicas.Genero).Distinct().ToList();
+
+            foreach (var genero in todosOsGenerosMusicais)
+            {
+                Console.WriteLine($"- {genero}");
+            }
+        }
+    }
+}
