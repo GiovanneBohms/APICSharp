f8f93728fd0ae8f7b9bb573c5149161b4209732b
Giovanne <giovannebohms@gmail.com>
2024-07-31 15:58:25 -0300

    HTTP

--- ScreenSound - 04/Program.cs
+++ ScreenSound - 04/Program.cs
@@ -1,8 +1,15 @@
-﻿using (HttpClient client = new HttpClient()){
+﻿using ScreenSound___04.Modelos;
+using System.Text.Json;
+using ScreenSound___04.Filtros;
+
+using (HttpClient client = new HttpClient()){
     try
     {
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.jso");
-    Console.WriteLine(resposta);
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[0].ExibirDetalhesDaMusica();
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     }
     catch (Exception ex)
     {
