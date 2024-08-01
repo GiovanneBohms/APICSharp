5426a15442b0c127f7a71817bfc7f41f453edb85
Giovanne <giovannebohms@gmail.com>
2024-08-01 16:52:51 -0300

    Filter Order

---
+++ ScreenSound - 04/Filtros/LinqOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScreenSound___04;
+using ScreenSound___04.Modelos;
+namespace ScreenSound___04.Filtros
+{
+    internal class LinqOrder
+    {
+        public static void ExibirListaDeArtistasOrdernados(List<Musica> musicas)
+        {
+            var artistasOrdernados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+            Console.WriteLine( "Lista de artistas ordernados");
+            foreach (var artista in artistasOrdernados)
+            {
+                Console.WriteLine($"- {artista}");
+            }
+        }
+    }
+}
